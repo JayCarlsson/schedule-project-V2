4761e7ce83d21e2d0249b0983bb97d968cff0d93
JayCarlsson <jay.carlsson06@icloud.com>
2026-02-22 15:24:47 +0100

    Test

--- frontend/script.js
+++ frontend/script.js
@@ -51,6 +51,8 @@ const tooltip = document.getElementById("tooltip");
 
 // Map of shift codes to place/time
 const shiftReference = {
+  // ---- Sälen ----
+  
   "HL1": "Harrys Lindvallen 1: 22.00-01.15",
   "HT1": "Harrys Tandådalen 1: 22.00-02.15",
   "Hö1R": "Högfjällshotellet i Sälen 1: Pianobaren Roulette 20:00-02:15",
@@ -65,8 +67,57 @@ const shiftReference = {
   "Tv": "Tvillingpass",
   "Wä1": "Wärsan 1: 22.00-02.15",
   "Wä2": "Wärsan 3: Wärsan bord 2 22.00-02.15",
-  "WäA": "Wärsan 2: Dagspass afterski 15.00-17.45"
+  "WäA": "Wärsan 2: Dagspass afterski 15.00-17.45",
+
+  // ---- Gothenburg ----
+
+  "8T": "8ight 1: Fre & Lör 23.00-03.15 (kan ha öppet till 04) || 26/2 22.00-02.15 || 27/2 22.00-03.15",
+  "AD": "Annat Distrikt",
+  "BA": "Bar Petite 1: Fre & Lör 23.00-02.15 (kan ha öppet till 03)",
+  "BP1": "Biljardpalatset Göteborg 1: Fre & Lör 19.00-03.15",
+  "BP2": "Biljardpalatset Göteborg 2: Fre 22.00-03.15 || Lör 21.00-03.15",
+  "DU1": "Irish Embassy 1: Sön-Tors 22.00-03.15 || Fre & Lör 21.00-05.15",
+  "DU2": "Irish Embassy 2: Fre & Lör 22.00-05.15",
+  "DU3": "Irish Embassy 3: Fre & Lör 23.45-05.15",
+  "EV": "Event",
+  "EX1": "Excet 1: Fre & Lör 23.00-05.15",
+  "EX2": "Excet 2: Fre & Lör 00.00-05.15 || Inlogg Joker 23:59",
+  "EX3": "Excet 3: Fre & Lör 00.00-05.15 || 24/2 22.00-02.15 || Inlogg Joker 23:59",
+  "EX4": "Excet 4: Fre & Lör 23.00-05.15 || 24/2 22.00-02.15",
+  "Gr1": "Grand Bingo 1: Mån-Tors 18.00-23.00 || Fre & Lör 18.00-02.00 || Kontanter || Stängning börjar 22.15 vardagar, 01.15 helger",
+  "Gr2": "Grand Bingo 2: Mån-Tors 18.00-23.00 || Fre & Lör 18.00-02.00 || Kontanter || Stängning börjar 22.15 vardagar, 01.15 helger",
+  "Gr4": "Grand Bingo 4: Fre & Lör 19.00-01.30 || Kontanter || Stängning börjar 01.15",
+  "Gr5": "Grand Bingo 5: Fre & Lör 20.00-01.30 || Kontanter || Stängning börjar 01.15",
+  "GS": "Gillestugan 1: Fre & Lör 22.00-03.15",
+  "JA": "Jacy'z: Tis 20.00-00.15 || Tors & Sön 19.00-23.15 || Fre & Lör 18.00-21.00 || Alternativt 19.00-23.00 eller 21.00-01.15 beroende på pass",
+  "JS": "John Scotts Mölndal 1: Fre & Lör 20.00-01.15",
+  "JSA": "John Scotts Avenyn 1: Fre & Lör 20.00-02.15",
+  "Ka1": "Kajskjul 105 1: 21/2 18.00-03.15",
+  "Ka2": "Kajskjul 105 2: 21/2 18.00-03.15",
+  "Ka3": "Kajskjul 105 3: 21/2 21.30-03.15 (rastar bord 1 i 30 min)",
+  "Ka4": "Kajskjul 105 4: 21/2 21.30-03.15 (rastar bord 2 i 30 min)",
+  "LB": "Lion Bar: Fre & Lör 21.00-03.15",
+  "Li1": "Lilla London 1: Ons & Tors 20.00-00.15 || Fre & Lör 19.00-03.15 (kan ha öppet till 04)",
+  "Li2": "Lilla London 2: Fre & Lör 22.00-03.15 (kan ha öppet till 04)",
+  "Li3": "Lilla London 3: Fre & Lör 22.00-03.15 (kan ha öppet till 04)",
+  "OL": "Olearys Järntorget 1: Fre & Lör 21.00-01.15",
+  "OP": "Ospecificerat Pass: Ring schematelefon kl 16.00 (0708-85 15 55)",
+  "PL1": "Park Lane 1: Fre & Lör 23.00-05.15 || Sön 23.00-03.15",
+  "PL2": "Park Lane 2: Fre & Lör 23.00-05.15 || Sön 23.00-03.15",
+  "PZ": "Plaza Göteborg 1: Fre & Lör 20.00-00.15 (kan ha öppet till 01) || Platsnummer 17092",
+  "RB1": "Rockbaren 1: Tors 22.00-03.15 || Fre & Lör 21.00-03.15 (kan ha öppet till 04) || 25/2 21.30-03.15",
+  "RB2": "Rockbaren 2: 25/2 21.30-03.15",
+  "Si": "Sing Sing Göteborg 1: Fre & Lör 22.00-03.15",
+  "SÄ": "Säsong",
+  "TG": "TG Valandhuset 1: Fre & Lör 22.00-02.15 (kan ha öppet till 03/04) || Platsnummer 17107 || Kodlås 170",
+  "TP": "Tvillingpass",
+  "TR1": "Trädgårn Göteborg 1: 6/2 23.00-04.15 || 7/2 22.00-02.15 || 14/2 23.00-03.15 || 23-24/2 22.00-02.15 || 27/2 23.00-03.15",
+  "TR2": "Trädgårn Göteborg 2: 6/2 23.00-04.15 || 23-24/2 22.00-02.15 || 27/2 23.00-03.15",
+  "VA": "Valand 1: Fre & Lör 23.00-03.15 (kan ha öppet till 04/05)",
+  "VI": "Viiva 1: Fre & Lör 21.00-01.30 (kan ha öppet till 02)",
+  "ÅB1": "Åby 1: 14/2 15.00-21.15"
 };
+
 
 // Event delegation: works for dynamic badges
 document.getElementById("calendar").addEventListener("mouseover", (e) => {
